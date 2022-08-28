eec07810622f7e1078892e2b579497c439165b2f
Rudra Shende <63746650+hypercoder457@users.noreply.github.com>
2022-08-28 13:13:13 -0400

    step 1: basic layout of app

--- App.tsx
+++ App.tsx
@@ -1,9 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Platform, Image, TouchableOpacity, Dimensions } from 'react-native';
 
 export default function App() {
+  const appTextMargin = Platform.select({
+    ios: {
+      marginTop: 30
+    },
+    android: {
+      marginTop: 30
+    },
+    web: {
+      marginTop: 10
+    }
+  });
   return (
     <View style={styles.container}>
-      <Text>Water intake Tracker</Text>
+      <Text style={appTextMargin}>Water intake Tracker</Text>
+      <TouchableOpacity>
+        <Text style={styles.plusButton}>+</Text>
+      </TouchableOpacity>
+      <Image source={require('./assets/glass-of-water.jpg')} style={styles.mainAppImage} />
+      <TouchableOpacity>
+        <Text style={{marginTop: 400}}>-</Text>
+      </TouchableOpacity>
+      <StatusBar style='dark' />
     </View>
   );
 }
@@ -13,6 +33,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  mainAppImage: {
+    width: '50%',
+    height: '50%',
+    marginTop: 100,
+    position: 'absolute',
+    resizeMode: 'cover'
+  },
+  plusButton: { marginTop: 40 }
 });
